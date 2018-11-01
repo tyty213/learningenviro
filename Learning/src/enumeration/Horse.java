e42415de8dec1fa293489c8c51e386afa2bd3163
Tylor <Tylor@192.168.1.27>
2018-11-01 23:57:25 +0000

    Using the EnumSet.range method

--- Learning/src/enumeration/Horse.java
+++ Learning/src/enumeration/Horse.java
@@ -4,7 +4,10 @@ public enum Horse {
 
 	Tylor("funny", "17"),
 	Kayleigh("cunt", "16"),
-	Mitchell("Junkie", "18");
+	Mitchell("Junkie", "18"),
+	Steven("Auld", "28?"),
+	Susan("Lecturer", "30?"),
+	Lewis("Bro", "16");
 
 	private final String DESC;
 	private final String AGE;
--- Learning/src/enumeration/Mouse.java
+++ Learning/src/enumeration/Mouse.java
@@ -1,11 +1,21 @@
 package enumeration;
 
+import java.util.EnumSet;
+
 public class Mouse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		for(Horse people: Horse.values()) {
+			
+			System.out.printf("%s \t %s \t %s \n", people, people.getDesc(), people.getAge());
+			
+		}
+		
+		System.out.println("\n Range of constants \n");
+		
+		for(Horse people : EnumSet.range(Horse.Kayleigh, Horse.Susan)) {
 			
 			System.out.printf("%s \t %s \t %s \n", people, people.getDesc(), people.getAge());
 			
