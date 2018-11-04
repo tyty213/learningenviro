eafee963796296b6c961ba122b77068dfb7b0d04
Tylor Graham <tylorgraham1231@gmail.com>
2018-11-04 23:55:44 +0000

    using event handling and action listeners

--- Learning/src/gui/Horse.java
+++ Learning/src/gui/Horse.java
@@ -1,22 +1,38 @@
 package gui;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
 
 public class Horse extends JFrame{
 
-	private JLabel item1;
+	private JTextField item1;
+	private JTextField item2;	
+	private JTextField item3;
+	
+	private JPasswordField passwordField;
 	
 	public Horse() {
 		
-		super("The title bar");
+		super("The title");
 		setLayout(new FlowLayout());
 		
-		item1 = new JLabel("This is a sentence.");
-		item1.setToolTipText("This is gonna show when you hover");
+		item1 = new JTextField(10);
 		add(item1);
 		
+		item2 = new JTextField("Enter text here");
+		add(item2);
+		
+		item3 = new JTextField("uneditable", 20);
+		item3.setEditable(false);
+		add(item3);
+		
+		passwordField = new JPasswordField("My pass");
+		add(passwordField);
+		
 	}
-	
 }
