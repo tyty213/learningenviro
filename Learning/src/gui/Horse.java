eb36f7b970d769af2ee697bb5a91b04e8ef02bf6
Tylor Graham <tylorgraham1231@gmail.com>
2018-11-04 23:32:40 +0000

    Using JFrame

--- Learning/src/gui/Horse.java
+++ Learning/src/gui/Horse.java
@@ -1,5 +1,22 @@
 package gui;
 
-public class Horse {
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class Horse extends JFrame{
+
+	private JLabel item1;
+	
+	public Horse() {
+		
+		super("The title bar");
+		setLayout(new FlowLayout());
+		
+		item1 = new JLabel("This is a sentence.");
+		item1.setToolTipText("This is gonna show when you hover");
+		add(item1);
+		
+	}
+	
 }
--- Learning/src/gui/Mouse.java
+++ Learning/src/gui/Mouse.java
@@ -1,21 +1,16 @@
 package gui;
 
-import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 
 public class Mouse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String fNum = JOptionPane.showInputDialog("Enter First Number");
-		String sNum = JOptionPane.showInputDialog("Enter Second Number");
-		
-		int num1 = Integer.parseInt(fNum);
-		int num2 = Integer.parseInt(sNum);
-		
-		int sum = num1+num2;
-		JOptionPane.showMessageDialog(null, "The sum is " + sum, "Answer", JOptionPane.PLAIN_MESSAGE);
-		
+		Horse window1 = new Horse();
+		window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window1.setSize(275, 180);
+		window1.setVisible(true);
 	}
 
 }
