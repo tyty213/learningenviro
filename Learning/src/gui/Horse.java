51102a30a9ba12b84ee31742c5249550a97e88da
Tylor Graham <tylorgraham1231@gmail.com>
2018-11-05 00:15:28 +0000

    .

--- Learning/src/gui/Horse.java
+++ Learning/src/gui/Horse.java
@@ -48,9 +48,9 @@ public class Horse extends JFrame{
 			
 			String string = "";
 			
-			if(event.getSource() == item1) 		string = String.format("field 1 : %s", event.getActionCommand());
-			else if(event.getSource() == item2) string = String.format("field 2 : %s", event.getActionCommand());
-			else if(event.getSource() == item3) string = String.format("field 3 : %s", event.getActionCommand());
+			if(event.getSource() == item1) 				string = String.format("field 1 : %s", event.getActionCommand());
+			else if(event.getSource() == item2)			string = String.format("field 2 : %s", event.getActionCommand());
+			else if(event.getSource() == item3) 		string = String.format("field 3 : %s", event.getActionCommand());
 			else if(event.getSource() == passwordField) string = String.format("password field if %s", event.getActionCommand());
 			
 			JOptionPane.showMessageDialog(null, string);
