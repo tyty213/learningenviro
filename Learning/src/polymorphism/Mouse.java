0202dccd4842c593cdd9847e0357e73e2b295ee1
Tylor Graham <tylorgraham1231@gmail.com>
2018-11-05 14:04:40 +0000

    Full polymorphism example

--- Learning/src/polymorphism/Mouse.java
+++ Learning/src/polymorphism/Mouse.java
@@ -7,9 +7,22 @@ public class Mouse {
 		Hunter tylor = new Hunter();
 		Animal animalObj = new Animal();
 		Animal dogObj = new Dog();
+		Animal cowObj = new Cow();
 		
 		tylor.digest(animalObj);
 		tylor.digest(dogObj);
+		tylor.digest(cowObj);
 		
+		System.out.println();
+		
+		Animal eat[] = new Animal[3];
+		eat[0] = new Dog();
+		eat[1] = new Horse();
+		eat[2] = new Cow();
+		
+		for(Animal a : eat) {
+			
+			a.eat();
+		}
 	}
 }
